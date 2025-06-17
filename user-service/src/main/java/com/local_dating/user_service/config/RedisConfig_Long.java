2a9ee03bebec5ab89c2b449d45d340bf9d4da920
macaw <temkhan@naver.com>
2025-06-17 21:59:51 +0900

    refactor: 인증번호 확인 코드 수정

---
+++ user-service/src/main/java/com/local_dating/user_service/config/RedisConfig_Long.java
@@ -0,0 +1,28 @@
+package com.local_dating.user_service.config;
+
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericToStringSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Configuration
+@EnableCaching
+public class RedisConfig_Long {
+
+    @Bean
+    public RedisTemplate<String, Long> redisStringLongTemplate(RedisConnectionFactory redisConnectionFactory) {
+        RedisTemplate<String, Long> template = new RedisTemplate<>();
+        template.setConnectionFactory(redisConnectionFactory);
+
+        template.setKeySerializer(new StringRedisSerializer());
+        template.setValueSerializer(new GenericToStringSerializer<>(Long.class));
+        template.setHashKeySerializer(new StringRedisSerializer());
+        template.setHashValueSerializer(new GenericToStringSerializer<>(Long.class));
+
+        template.afterPropertiesSet();
+        return template;
+    }
+}
