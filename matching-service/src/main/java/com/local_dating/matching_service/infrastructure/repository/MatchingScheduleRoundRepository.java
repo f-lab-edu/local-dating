f2e2924c66067d2b26707b251ac94c14cda0347e
macaw <temkhan@naver.com>
2026-05-06 21:56:44 +0900

    refactor: 매칭검증 수정, 매칭 스케줄 요청 수정

--- matching-service/src/main/java/com/local_dating/matching_service/infrastructure/repository/MatchingScheduleRoundRepository.java
+++ matching-service/src/main/java/com/local_dating/matching_service/infrastructure/repository/MatchingScheduleRoundRepository.java
@@ -4,10 +4,15 @@ import com.local_dating.matching_service.domain.entity.MatchingScheduleRound;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface MatchingScheduleRoundRepository extends JpaRepository<MatchingScheduleRound, Long> {
 
     Optional<MatchingScheduleRound> findByIdAndMatchingIdAndStatusCd(Long id, Long matchingId, String statusCd);
+
+    List<MatchingScheduleRound> findByIdInAndMatchingIdInAndStatusCd(List<Long> idList, List<Long> matchingIdList, String statusCd);
+
+    Optional<MatchingScheduleRound> findByMatchingIdAndRound(Long matchingId, Long round);
 }
